43efa5443aa65828ffe01612f87add1456d02d66
shubham kumar <67566562+ASTECHUB@users.noreply.github.com>
2021-03-28 17:05:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Form Tutorial</title>
+</head>
+<body>
+<div class="container mt-6">
+<h1> <strong>Contact form validation with php</strong></h1>
+<form action="index.php" method="post" >
+  <div class="mb-3">
+    <label for="Email" class="form-label">Email address</label>
+    <input type="email" name="Email" class="form-control" id="Email" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="pass" class="form-label">Password</label>
+    <input type="password" class="form-control" id="pass" name="pass">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+<div>
+
+<?php
+//both is right ......
+//if(isset($_POST)){
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $email=$_POST['Email'];
+    $pswd = $_POST['pass'];
+}
+  mail("shubham1x23@gmail.com", "Astechub Contact Form", $email,$pswd);
+echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+<strong>Scucess! </strong>  your email'.$email.' and password '.$pswd.' has been submited!
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+?>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</html>
